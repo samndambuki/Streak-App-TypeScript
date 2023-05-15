1f2cec5b433cbc026e71844aeaea497a11b2457d
samndambuki <sam.ndambuki08@gmail.com>
2023-05-15 18:53:35 +0300

    final commit

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-var Habit = /** @class */ (function () {
-    function Habit(name, stoppedDate) {
+"use strict";
+class Habit {
+    constructor(name, stoppedDate) {
         this.name = name;
         this.stoppedDate = stoppedDate;
     }
-    Habit.prototype.getDaysSinceStopped = function () {
-        var currentDate = new Date();
-        var timeDifference = currentDate.getTime() - this.stoppedDate.getTime();
-        var days = Math.floor(timeDifference / (1000 * 3600 * 24));
+    getDaysSinceStopped() {
+        const currentDate = new Date();
+        const timeDifference = currentDate.getTime() - this.stoppedDate.getTime();
+        const days = Math.floor(timeDifference / (1000 * 3600 * 24));
         return days;
-    };
-    return Habit;
-}());
-var StreakApp = /** @class */ (function () {
-    function StreakApp() {
-        this.habits = [];
     }
-    StreakApp.prototype.addHabit = function (name, stoppedDate) {
-        var habit = new Habit(name, stoppedDate);
+    getStoppedDateTime() {
+        const options = { year: 'numeric', month: 'long', weekday: 'long', day: 'numeric' };
+        return this.stoppedDate.toLocaleString('en-US', options);
+    }
+}
+class StreakApp {
+    constructor() {
+        this.habits = [];
+        this.habitList = document.getElementById('habitList');
+    }
+    addHabit(name, stoppedDate) {
+        const habit = new Habit(name, stoppedDate);
         this.habits.push(habit);
-    };
-    StreakApp.prototype.displayHabits = function () {
-        this.habits.forEach(function (habit, index) {
-            console.log("Habit ".concat(index + 1, ": ").concat(habit.name));
+    }
+    displayHabits() {
+        if (!this.habitList)
+            return;
+        this.habitList.innerHTML = '';
+        this.habits.forEach((habit, index) => {
+            const habitElement = document.createElement('div');
+            habitElement.classList.add('habit');
+            habitElement.innerHTML = `
+          <h3>Habit ${index + 1}: ${habit.name}</h3>
+          <p>Stopped Time: ${habit.getStoppedDateTime()}</p>
+          <p>Days since stopped: ${habit.getDaysSinceStopped()}</p>
+        `;
+            this.habitList.appendChild(habitElement);
         });
-    };
-    StreakApp.prototype.getDaysSinceStopped = function (habitIndex) {
-        var habit = this.habits[habitIndex - 1];
-        if (habit) {
-            return habit.getDaysSinceStopped();
-        }
-        return -1; // Return -1 if habit index is invalid
-    };
-    return StreakApp;
-}());
-var app = new StreakApp();
-// Add habits
-var smokingDate = new Date('2023-04-28');
-app.addHabit('Smoking', smokingDate);
-var exerciseDate = new Date('2023-04-15');
-app.addHabit('Exercise', exerciseDate);
-// Display habits
+    }
+}
+const app = new StreakApp();
+app.addHabit('Smoking', new Date('2023-04-28'));
+app.addHabit('Exercise', new Date('2023-04-15'));
+app.addHabit('Take a walk', new Date('2023-03-10'));
+app.addHabit('Cycling', new Date('2021-02-15'));
+app.addHabit('Healthy Eating', new Date('2020-04-14'));
 app.displayHabits();
-// Get days since stopped for a habit
-var habitIndex = 1; // Change this to the desired habit index
-var daysSinceStopped = app.getDaysSinceStopped(habitIndex);
-if (daysSinceStopped !== -1) {
-    console.log("Days since stopped: ".concat(daysSinceStopped));
-}
-else {
-    console.log('Invalid habit index.');
-}
--- app.ts
+++ app.ts
@@ -1,65 +1,67 @@
 class Habit {
-  name: string;
-  stoppedDate: Date;
-
-  constructor(name: string, stoppedDate: Date) {
-    this.name = name;
-    this.stoppedDate = stoppedDate;
-  }
-
-  getDaysSinceStopped(): number {
-    const currentDate = new Date();
-    const timeDifference = currentDate.getTime() - this.stoppedDate.getTime();
-    const days = Math.floor(timeDifference / (1000 * 3600 * 24));
-    return days;
-  }
-}
-
-class StreakApp {
-  habits: Habit[];
-
-  constructor() {
-    this.habits = [];
-  }
-
-  addHabit(name: string, stoppedDate: Date): void {
-    const habit = new Habit(name, stoppedDate);
-    this.habits.push(habit);
-  }
-
-  displayHabits(): void {
-    this.habits.forEach((habit, index) => {
-      console.log(`Habit ${index + 1}: ${habit.name}`);
-    });
-  }
-
-  getDaysSinceStopped(habitIndex: number): number {
-    const habit = this.habits[habitIndex - 1];
-    if (habit) {
-      return habit.getDaysSinceStopped();
+    name: string;
+    stoppedDate: Date;
+  
+    constructor(name: string, stoppedDate: Date) {
+      this.name = name;
+      this.stoppedDate = stoppedDate;
     }
-    return -1; // Return -1 if habit index is invalid
+  
+    getDaysSinceStopped(): number {
+      const currentDate = new Date();
+      const timeDifference = currentDate.getTime() - this.stoppedDate.getTime();
+      const days = Math.floor(timeDifference / (1000 * 3600 * 24));
+      return days;
+    }
+
+     getStoppedDateTime(): string {
+    const options = { year: 'numeric', month: 'long', weekday: 'long', day: 'numeric' };
+    return this.stoppedDate.toLocaleString('en-US', options);
   }
-}
 
-const app = new StreakApp();
-
-// Add habits
-const smokingDate = new Date('2023-04-28');
-app.addHabit('Smoking', smokingDate);
-
-const exerciseDate = new Date('2023-04-15');
-app.addHabit('Exercise', exerciseDate);
-
-// Display habits
-app.displayHabits();
-
-// Get days since stopped for a habit
-const habitIndex = 1; // Change this to the desired habit index
-const daysSinceStopped = app.getDaysSinceStopped(habitIndex);
-if (daysSinceStopped !== -1) {
-  console.log(`Days since stopped: ${daysSinceStopped}`);
-} else {
-  console.log('Invalid habit index.');
-}
-
+    
+  }
+  
+  class StreakApp {
+    habits: Habit[];
+    habitList: HTMLElement | null;
+  
+    constructor() {
+      this.habits = [];
+      this.habitList = document.getElementById('habitList');
+    }
+  
+    addHabit(name: string, stoppedDate: Date): void {
+      const habit = new Habit(name, stoppedDate);
+      this.habits.push(habit);
+    }
+  
+    displayHabits(): void {
+      if (!this.habitList) return; 
+  
+      this.habitList.innerHTML = ''; 
+  
+      this.habits.forEach((habit, index) => {
+        const habitElement = document.createElement('div');
+        habitElement.classList.add('habit');
+        habitElement.innerHTML = `
+          <h3>Habit ${index + 1}: ${habit.name}</h3>
+          <p>Stopped Time: ${habit.getStoppedDateTime()}</p>
+          <p>Days since stopped: ${habit.getDaysSinceStopped()}</p>
+        `;
+        this.habitList.appendChild(habitElement);
+      });
+    }
+  }
+  
+  
+  
+  const app = new StreakApp();
+  
+  app.addHabit('Smoking', new Date('2023-04-28'));
+  app.addHabit('Exercise', new Date('2023-04-15'));
+  app.addHabit('Take a walk', new Date('2023-03-10'));
+  app.addHabit('Cycling', new Date('2021-02-15'));
+  app.addHabit('Healthy Eating', new Date('2020-04-14'));
+  app.displayHabits();
+  
